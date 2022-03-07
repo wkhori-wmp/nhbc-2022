a8af7da2116d4a75455526541389821a98a6e681
Cole Mitchell <cxm35v+1@amount.com>
2022-03-07 17:34:53 -0600

    additional tweaks to expand

--- src/components/views/playlist/Playlist.js
+++ src/components/views/playlist/Playlist.js
@@ -4,10 +4,12 @@ import { PlaylistWrapper } from './Playlist.style';
 import { getItems, deleteItem } from '../../../firebase/firebase';
 import BootstrapTable from 'react-bootstrap-table-next';
 
+const TRASH_URL = 'https://media.istockphoto.com/vectors/trash-cangarbage-canrubbish-bin-icon-vector-id928418914?k=20&m=928418914&s=612x612&w=0&h=hP_RnwBPgcWMJoHqGw9A12vwy5IY3fzfr2dXpx4m6j8=' 
+
 const Playlist = () => {
   // const classes = useStyles();
   let [mySongsArr, setmySongsArr] = useState([
-  {index: 1, title: 'Dream On', artist: 'Aerosmith', album: 'Aerosmith', ytLink: 'https://www.youtube.com/watch?v=89dGC8de0CA', delete: 'https://media.istockphoto.com/vectors/trash-cangarbage-canrubbish-bin-icon-vector-id928418914?k=20&m=928418914&s=612x612&w=0&h=hP_RnwBPgcWMJoHqGw9A12vwy5IY3fzfr2dXpx4m6j8='}
+  {index: 1, title: 'Dream On', artist: 'Aerosmith', album: 'Aerosmith', ytLink: 'https://www.youtube.com/watch?v=89dGC8de0CA', delete: TRASH_URL}
   ]);
 
   const getPlaylist = () => {
@@ -38,7 +40,7 @@ const Playlist = () => {
   }
 
   function imageFormatter(cell, row) {
-    return <div style={{cursor: 'pointer'}} onClick={() => console.log('delete this song', row)}>
+    return <div style={{cursor: 'pointer'}} onClick={() => setmySongsArr([])}>
             <img src={cell} height={20}/>
           </div> ;
   }
@@ -70,7 +72,7 @@ const Playlist = () => {
     <PlaylistWrapper>
       <h1>Playlist</h1>
       <BootstrapTable 
-          noDataIndication={() => console.log('No Data or clothes!')}
+          noDataIndication={() => console.log('No Data at the moment... to be implemented later')}
           keyField='index'
           data={ mySongsArr }
           columns={ columns }
