dc8560f5b7d876b80be82c71deef344e38417fd9
Cole Mitchell <w.coleman.mitchell@gmail.com>
2022-04-08 13:17:33 -0500

    Remove dummy data, remove index column from `Playlist` table

--- src/components/views/playlist/Playlist.js
+++ src/components/views/playlist/Playlist.js
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { CSVLink } from 'react-csv';
-import { PlaylistWrapper } from './Playlist.style';
-import { getItems, deleteItem } from '../../../firebase/firebase';
-import BootstrapTable from 'react-bootstrap-table-next';
-import styled from 'styled-components';
-import { Trash, Trash2 } from 'react-feather';
-
-const TRASH_URL = 'https://media.istockphoto.com/vectors/trash-cangarbage-canrubbish-bin-icon-vector-id928418914?k=20&m=928418914&s=612x612&w=0&h=hP_RnwBPgcWMJoHqGw9A12vwy5IY3fzfr2dXpx4m6j8=';
+import React, { useEffect, useState } from "react";
+import { CSVLink } from "react-csv";
+import { PlaylistWrapper } from "./Playlist.style";
+import { getItems, deleteItem } from "../../../firebase/firebase";
+import BootstrapTable from "react-bootstrap-table-next";
+import styled from "styled-components";
+import { Trash2 } from "react-feather";
 
 const YoutubeVideo = styled.iframe`
   width: 560px;
@@ -18,18 +16,14 @@ const YoutubeVideo = styled.iframe`
 `;
 
 const TrashIconWrapper = styled.div`
-&:hover .delete-button {
-  color: red;
-}
-`
+  &:hover .delete-button {
+    color: red;
+  }
+`;
 
 const Playlist = () => {
   // const classes = useStyles();
-  let [mySongsArr, setmySongsArr] = useState([
-  {index: 1, title: 'Dream On', artist: 'Aerosmith', album: 'Aerosmith', ytLink: 'https://www.youtube.com/watch?v=89dGC8de0CA', delete: TRASH_URL},
-  {index: 2, title: 'Beat It', artist: 'Michael Jackson', album: 'Thriller', ytLink: 'https://www.youtube.com/watch?v=oRdxUFDoQe0', delete: TRASH_URL},
-  {index: 3, title: 'Bohemian Rhapsody', artist: 'Queen', album: 'A Night at the Opera', ytLink: 'https://www.youtube.com/watch?v=fJ9rUzIMcZQ', delete: TRASH_URL}
-  ]);
+  let [mySongsArr, setmySongsArr] = useState([]);
 
   const getPlaylist = () => {
     getItems().then(function (result) {
@@ -37,14 +31,20 @@ const Playlist = () => {
         Object.keys(result).map((item) => ({ ...result[item], id: item }))
       );
     });
-  }
+  };
 
   const expandRow = {
-    renderer: row => (
-      <div style={{display: 'flex',  justifyContent:'center'}}>
-        <YoutubeVideo src={'https://youtube.com/embed/'+row.ytLink.split('?v=')[1]} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></YoutubeVideo>
+    renderer: (row) => (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <YoutubeVideo
+          src={"https://youtube.com/embed/" + row.ytLink.split("?v=")[1]}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></YoutubeVideo>
       </div>
-    )
+    ),
   };
 
   // Use effect hook with an empty dependency array [], will run only on mounted
@@ -56,72 +56,82 @@ const Playlist = () => {
   const handleDelete = (id) => {
     deleteItem(id);
     getPlaylist();
-  }
+  };
 
   const removeSong = (index) => {
-    let newSongArr = mySongsArr.filter(a => a.index !== index)
-    setmySongsArr(newSongArr)
-  }
+    let newSongArr = mySongsArr.filter((a) => a.index !== index);
+    setmySongsArr(newSongArr);
+  };
 
   function imageFormatter(cell, row) {
-    return (<TrashIconWrapper
-              style={{cursor: 'pointer'}}
-              onClick={() => { removeSong(row.index) }}>
-            <Trash2 className='delete-button'/>
-          </TrashIconWrapper>);
+    return (
+      <TrashIconWrapper
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          removeSong(row.index);
+        }}
+      >
+        <Trash2 className="delete-button" />
+      </TrashIconWrapper>
+    );
   }
 
-  const columns = [{
-    dataField: 'index',
-    text: '#',
-    align: 'center',
-    headerAlign: 'center',
-    sort: true,
-    style: { cursor: 'pointer' }
-  }, {
-    dataField: 'title',
-    align: 'center',
-    text: 'Song Title',
-    headerAlign: 'center',
-    sort: true,
-    style: { cursor: 'pointer' }
-  }, {
-    dataField: 'artist',
-    align: 'center',
-    text: 'Artist',
-    headerAlign: 'center',
-    sort: true,
-    style: { cursor: 'pointer' }
-  }, {
-    dataField: 'delete',
-    align: 'center',
-    text: 'Delete?',
-    headerAlign: 'center',
-    formatter: imageFormatter,
-    formatExtraData: mySongsArr,
-    style: { cursor: 'pointer' }
-  }];
-
-  const csvHeaders = [
-    { label: 'Song Title', key: 'title' },
-    { label: 'Artist', key: 'artist' },    
-    { label: 'Album', key: 'album' },
+  const columns = [
+    {
+      dataField: "title",
+      align: "center",
+      text: "Song Title",
+      headerAlign: "center",
+      sort: true,
+      style: { cursor: "pointer" },
+    },
+    {
+      dataField: "artist",
+      align: "center",
+      text: "Artist",
+      headerAlign: "center",
+      sort: true,
+      style: { cursor: "pointer" },
+    },
+    {
+      dataField: "delete",
+      align: "center",
+      text: "Delete?",
+      headerAlign: "center",
+      formatter: imageFormatter,
+      formatExtraData: mySongsArr,
+      style: { cursor: "pointer" },
+    },
   ];
 
-  const emptyDataMessage = () => { return <div style={{display: 'flex',  justifyContent:'center'}}>No data to display... go add some songs to the playlist!</div>}
+  const csvHeaders = [
+    { label: "Song Title", key: "title" },
+    { label: "Artist", key: "artist" },
+    { label: "Album", key: "album" },
+  ];
+
+  const emptyDataMessage = () => {
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        No data to display... go add some songs to the playlist!
+      </div>
+    );
+  };
 
   return (
     <PlaylistWrapper>
       <h1>Playlist</h1>
-      <BootstrapTable 
-          style={{cursor: 'pointer'}}
-          noDataIndication={emptyDataMessage}
-          keyField='index'
-          data={ mySongsArr }
-          columns={ columns }
-          expandRow={ expandRow }
+      <BootstrapTable
+        style={{ cursor: "pointer" }}
+        noDataIndication={emptyDataMessage}
+        keyField="index"
+        data={mySongsArr}
+        columns={columns}
+        expandRow={expandRow}
       />
-      <CSVLink data={mySongsArr} headers={csvHeaders}>Export your playlist to a CSV</CSVLink>
+      <CSVLink data={mySongsArr} headers={csvHeaders}>
+        Export your playlist to a CSV
+      </CSVLink>
     </PlaylistWrapper>
   );
 };
