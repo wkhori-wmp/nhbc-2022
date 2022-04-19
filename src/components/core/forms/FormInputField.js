1e441226dd4ee282c878f3bc60046a18175a5884
Cole Mitchell <w.coleman.mitchell@gmail.com>
2022-04-19 10:51:50 -0500

    fix jumping error msgs

--- src/components/core/forms/FormInputField.js
+++ src/components/core/forms/FormInputField.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const StyledInput = styled.input`
   width: 100%;
   padding: 12px 20px;
-  margin: 8px 8px 20px;
+  margin: 8px 8px;
   box-sizing: border-box;
   border-radius: 10px;
   border-color: #bfbfbf;
@@ -15,18 +15,20 @@ const StyledInput = styled.input`
 const StyledErrorMessage = styled.span`
   color: red;
   margin-bottom: 20px;
+  min-height: 20px;
   margin-left: 8px;
 `;
 
 const FormInputField = ({ name, errors, validations = {}, ...rest }) => {
   // Get form context passed down from FormProvider
   const { register } = useFormContext();
+  const displayError = errors && name in errors;
   return (
     <>
       <StyledInput {...register(name, validations)} {...rest} />
-      {errors && name in errors && (
-        <StyledErrorMessage>{errors[name].message}</StyledErrorMessage>
-      )}
+      <StyledErrorMessage>
+        {displayError ? errors[name]?.message : ""}
+      </StyledErrorMessage>
     </>
   );
 };
