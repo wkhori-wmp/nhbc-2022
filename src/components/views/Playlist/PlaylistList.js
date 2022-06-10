090de7e91f6c9f165304fe457622cc6f5378baa9
Cole Mitchell <w.coleman.mitchell@gmail.com>
2022-06-10 16:10:08 -0500

    rip out playlist.map fcn

--- src/components/views/Playlist/PlaylistList.js
+++ src/components/views/Playlist/PlaylistList.js
@@ -20,35 +20,34 @@ const PlaylistList = ({ playlists }) => {
 
   return (
     <>
-      {playlists.map((playlist) => (
-        <ExistingPlaylistRow>
-          <span style={{ flexGrow: 0.4 }}>
-            <strong>{playlist.name}</strong>
-          </span>
+      <ExistingPlaylistRow>
+        <span style={{ flexGrow: 0.4 }}>
+          <strong>{playlists[0].name}</strong>
+        </span>
 
-          <Button
-            variant="link"
-            className="btn-sm"
-            style={{ padding: "0px" }}
-            onClick={() => viewPlaylist(playlist.name, playlist.uuid)}
-          >
-            View Playlist
-          </Button>
-          <Button
-            variant="link"
-            className="btn-sm"
-            style={{ padding: "0px" }}
-            onClick={() => addSong(playlist.name, playlist.uuid)}
-          >
-            Add Song
-          </Button>
-
-          <span style={{ flexGrow: 0.1 }}>
-            {playlist.songCount} Song
-            {playlist.songCount !== 1 && "s"}
-          </span>
-        </ExistingPlaylistRow>
-      ))}
+        <Button
+          variant="link"
+          className="btn-sm"
+          style={{ padding: "0px" }}
+          onClick={() =>
+            viewPlaylist(playlists[0].name.name, playlists[0].uuid)
+          }
+        >
+          View Playlist
+        </Button>
+        <Button
+          variant="link"
+          className="btn-sm"
+          style={{ padding: "0px" }}
+          onClick={() => addSong(playlists[0].name, playlists[0].uuid)}
+        >
+          Add Song
+        </Button>
+        <span style={{ flexGrow: 0.1 }}>
+          {playlists[0].songCount} Song
+          {playlists[0].songCount !== 1 && "s"}
+        </span>
+      </ExistingPlaylistRow>
     </>
   );
 };
