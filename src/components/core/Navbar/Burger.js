7ba105779ae4469c2f7634d7223c929c52fc56a7
Cole Mitchell <w.coleman.mitchell@gmail.com>
2022-06-13 14:02:43 -0500

    init commit; create branch

--- src/components/core/Navbar/Burger.js
+++ src/components/core/Navbar/Burger.js
@@ -3,9 +3,13 @@ import RightNav from "./RightNav";
 import { StyledBurger } from "./Navbar.style";
 
 const Burger = () => {
-  const [open, setOpen] = useState(false);
+  const open = false;
   function toggleMenu() {
-    setOpen(!open);
+    /*
+     * For this TO DO, fix the broken toggleMenu function with the useState hook
+     * to keep track of whether the burger menu is open
+     */
+    console.log("this should open and close the burger menu");
   }
 
   return (
