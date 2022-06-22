66877f668bc4c19025256df6bd9e5f058550efc6
Patrick Mosebach <pat.mosebach@gmail.com>
2022-06-22 12:39:17 -0500

    todo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./components/views/App/App";
 import { BrowserRouter } from "react-router-dom";
 
+// TODO: change "root" and save the file. Observe what happens in the app
 ReactDOM.render(
   <BrowserRouter>
     <App />
