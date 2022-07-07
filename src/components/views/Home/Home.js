d6b29b852469528a16c7e32db3a85dc9ec0b98e9
wkhori-wmp <71779755+wkhori-wmp@users.noreply.github.com>
2022-07-07 11:08:25 -0500

    Update Home.js

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -9,14 +9,6 @@ const Home = () => {
           Welcome to WMP Playlist! You can search through song recommendations
           and add your own!
         </Description>
-        <iframe
-          title="gif"
-          src="https://giphy.com/embed/tqfS3mgQU28ko"
-          width="380"
-          height="260"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
       </DescriptionAndImage>
     </PageWrapper>
   );
